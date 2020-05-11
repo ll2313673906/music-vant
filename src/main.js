26d5ec369a751b687e6720ffd2161912272e93d8
ll2313673906 <3340624204@qq.com>
2020-05-11 23:05:15 +0800

    vant项目搭建

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入Vant
+import { Button } from 'vant'
+Vue.use(Button)
+import { Tabbar, TabbarItem } from 'vant'
+Vue.use(Tabbar).use(TabbarItem)
+import { NavBar } from 'vant'
+Vue.use(NavBar)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
